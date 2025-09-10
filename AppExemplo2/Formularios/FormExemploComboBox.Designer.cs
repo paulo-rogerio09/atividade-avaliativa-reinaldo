57df3dfc66aba5877bced9bd8758c6ed0895b680
paulo-rogerio09 <secundariarmg2007@gmail.com>
2025-09-09 21:10:50 -0400

    sistema de desconto sem leave e key up

--- AppExemplo2/Formularios/FormExemploComboBox.Designer.cs
+++ AppExemplo2/Formularios/FormExemploComboBox.Designer.cs
@@ -29,10 +29,16 @@
         private void InitializeComponent()
         {
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.clientecb = new System.Windows.Forms.ComboBox();
+            this.calcularbtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.valortxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.descontotxt = new System.Windows.Forms.TextBox();
+            this.resultadotxt = new System.Windows.Forms.TextBox();
+            this.percentuallb = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.novobtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label4
@@ -45,51 +51,119 @@
             this.label4.Text = "SISTEMA DE DESCONTOS";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox1
+            // clientecb
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(103, 99);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 26;
+            this.clientecb.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.clientecb.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.clientecb.FormattingEnabled = true;
+            this.clientecb.Items.AddRange(new object[] {
+            "Cliente Diamante: 25%",
+            "Cliente Ouro: 20%",
+            "Cliente VIP: 15%",
+            "Cliente Comum: 10%",
+            "Cliente Aleatório: (Percentual Livre)"});
+            this.clientecb.Location = new System.Drawing.Point(103, 99);
+            this.clientecb.Name = "clientecb";
+            this.clientecb.Size = new System.Drawing.Size(121, 21);
+            this.clientecb.TabIndex = 26;
+            this.clientecb.SelectedIndexChanged += new System.EventHandler(this.clientecb_SelectedIndexChanged);
             // 
-            // button1
+            // calcularbtn
             // 
-            this.button1.Location = new System.Drawing.Point(281, 347);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 27;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.calcularbtn.Location = new System.Drawing.Point(45, 309);
+            this.calcularbtn.Name = "calcularbtn";
+            this.calcularbtn.Size = new System.Drawing.Size(75, 23);
+            this.calcularbtn.TabIndex = 27;
+            this.calcularbtn.Text = "CALCULAR";
+            this.calcularbtn.UseVisualStyleBackColor = true;
+            this.calcularbtn.Click += new System.EventHandler(this.calcularbtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(100, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(172, 13);
             this.label1.TabIndex = 28;
-            this.label1.Text = "label1";
+            this.label1.Text = "SELECIONE O TIPO DE CLIENTE";
             // 
-            // textBox1
+            // valortxt
             // 
-            this.textBox1.Location = new System.Drawing.Point(67, 259);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 29;
+            this.valortxt.Location = new System.Drawing.Point(103, 184);
+            this.valortxt.Name = "valortxt";
+            this.valortxt.Size = new System.Drawing.Size(121, 20);
+            this.valortxt.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "VALOR DA COMPRA";
+            // 
+            // descontotxt
+            // 
+            this.descontotxt.Location = new System.Drawing.Point(103, 240);
+            this.descontotxt.Name = "descontotxt";
+            this.descontotxt.Size = new System.Drawing.Size(121, 20);
+            this.descontotxt.TabIndex = 31;
+            this.descontotxt.TextChanged += new System.EventHandler(this.descontotxt_TextChanged);
+            // 
+            // resultadotxt
+            // 
+            this.resultadotxt.Location = new System.Drawing.Point(103, 371);
+            this.resultadotxt.Name = "resultadotxt";
+            this.resultadotxt.Size = new System.Drawing.Size(121, 20);
+            this.resultadotxt.TabIndex = 32;
+            // 
+            // percentuallb
+            // 
+            this.percentuallb.AutoSize = true;
+            this.percentuallb.Location = new System.Drawing.Point(100, 224);
+            this.percentuallb.Name = "percentuallb";
+            this.percentuallb.Size = new System.Drawing.Size(96, 13);
+            this.percentuallb.TabIndex = 33;
+            this.percentuallb.Text = "% DE DESCONTO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(100, 355);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "VALOR COM DESCONTO";
+            // 
+            // novobtn
+            // 
+            this.novobtn.Location = new System.Drawing.Point(197, 309);
+            this.novobtn.Name = "novobtn";
+            this.novobtn.Size = new System.Drawing.Size(75, 23);
+            this.novobtn.TabIndex = 35;
+            this.novobtn.Text = "NOVO";
+            this.novobtn.UseVisualStyleBackColor = true;
             // 
             // FormExemploComboBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(363, 479);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.novobtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.percentuallb);
+            this.Controls.Add(this.resultadotxt);
+            this.Controls.Add(this.descontotxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.valortxt);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.calcularbtn);
+            this.Controls.Add(this.clientecb);
             this.Controls.Add(this.label4);
             this.Name = "FormExemploComboBox";
             this.Text = "FormExemploComboBox";
+            this.Load += new System.EventHandler(this.FormExemploComboBox_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,9 +171,15 @@
 
         #endregion
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox clientecb;
+        private System.Windows.Forms.Button calcularbtn;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox valortxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox descontotxt;
+        private System.Windows.Forms.TextBox resultadotxt;
+        private System.Windows.Forms.Label percentuallb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button novobtn;
     }
 }
--- AppExemplo2/Formularios/FormExemploComboBox.cs
+++ AppExemplo2/Formularios/FormExemploComboBox.cs
@@ -15,8 +15,78 @@ namespace AppExemplo2.Formularios
         public FormExemploComboBox()
         {
             InitializeComponent();
-           
+            descontotxt.Visible = false;
+            percentuallb.Visible = false;
         }
 
+        private void FormExemploComboBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcularbtn_Click(object sender, EventArgs e)
+        {
+            int tipoCliente = clientecb.SelectedIndex;
+            double valorCompra = Convert.ToDouble(valortxt.Text);
+            double valorComDesconto = 0;
+            switch (tipoCliente)
+            {
+                case 0://Cliente Diamante: 25%
+                    {
+                        valorComDesconto = valorCompra - valorCompra * 0.25;
+                        resultadotxt.Text = valorComDesconto.ToString("C2");
+                        break;
+                    }//fim case 0
+                case 1://Cliente Ouro: 20%
+                    {
+                        valorComDesconto = valorCompra - valorCompra * 0.20;
+                        resultadotxt.Text = valorComDesconto.ToString("C2");
+                        break;
+                    }//fim case 1
+                case 2://Cliente Ouro: 20%
+                    {
+                        valorComDesconto = valorCompra - valorCompra * 0.15;
+                        resultadotxt.Text = valorComDesconto.ToString("C2");
+                        break;
+                    }//fim case 1
+                case 3://Cliente Ouro: 20%
+                    {
+                        valorComDesconto = valorCompra - valorCompra * 0.10;
+                        resultadotxt.Text = valorComDesconto.ToString("C2");
+                        break;
+                    }//fim case 1
+                case 4://Cliente Ouro: 20%
+                    {
+                        
+                        double desconto = Convert.ToDouble(descontotxt.Text)/100;
+                        valorComDesconto = valorCompra - valorCompra * desconto;
+                        resultadotxt.Text = valorComDesconto.ToString("C2");
+                        
+
+                        break;
+                    }//fim case 1
+                default:
+                    {
+                        MessageBox.Show("SELECIONE UM CLIENTE!", "ADS/JIPA", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        break;
+                    }//fim da estrutura switch
+            }
+        }
+
+        private void descontotxt_TextChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void clientecb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int tipoCliente = clientecb.SelectedIndex;
+            if (tipoCliente == 4)
+            {
+                descontotxt.Visible = true; //para ficar visível
+                percentuallb.Visible = true;//para ficar visível
+            }
+        }
     }
 }
